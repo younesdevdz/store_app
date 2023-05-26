7f29318e54127efee13112cd688835080f8517ea
younes bettahar <younesdz03@gmail.com>
2023-05-26 11:18:53 +0200

    first shop web commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./container/header";
+import {BrowserRouter as Router  , Route  , Routes} from 'react-router-dom'
+import ProductComponents from "./container/productComponents";
+import ProductListing from "./container/productListing";
+import ProductDetail from "./container/productDetail";
+function App() { 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+return (
+  <div className="App">
+  <Header />
+ 
+  <Router>
+    <Routes>
+        <Route exact path="/" element={<ProductListing />} />
+        <Route exact path="/product/productId" element={<ProductDetail />} />
+       
+    </Routes>
+</Router>
+  </div>
+)
 }
-
 export default App;
